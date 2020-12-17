14c9b8e7e3df2d06a683ff092048f74571c5f62b
Anne Caves <1014251@student.sae.edu.au>
2020-12-17 23:28:50 +1100

    Level 1 Movement Started
    Assets added to Level 1 and level 2

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject nymphPrefab;
+
+    public int xPos;
+    public int zPos;
+    public int yPos;
+    public int enemyCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 25)
+        {
+            xPos = Random.Range(10, 35);
+            zPos = Random.Range(5, 10);
+            yPos = Random.Range(0, 4);
+            Instantiate(nymphPrefab, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(5);
+            enemyCount += 1;
+
+        }
+
+        if (enemyCount > 25)
+        {
+            SceneManager.LoadScene("Main Menu Scene");
+        }
+    }
+
+   
+
+
+   
+}
---
+++ Assets/Scripts/SpawnThrowableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnThrowableObject : MonoBehaviour
+{
+    public GameObject throwableObject;
+    public Transform spawnLocation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(throwableObject, spawnLocation.transform.position, spawnLocation.transform.rotation);
+    }
+
+  
+}
