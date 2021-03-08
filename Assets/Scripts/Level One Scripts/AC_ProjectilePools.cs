4e04ed1073823110a8a166a07994c10a327a1f75
Anne <1014251@student.sae.edu>
2021-03-08 14:28:58 +1100

    Revert "Level one Shooter Script edited"
    This reverts commit 82e110494e9333715cdf3c42d8e3100b95adf3dc.

---
+++ Assets/Scripts/Level One Scripts/AC_ProjectilePools.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool // creates new projectiles at the start of the level
+{   
+    public List<T> Create<T>(GameObject prefab, int count)
+        where T : MonoBehaviour
+    
+    {//new lists of projectile objects for the functionality of the objects
+        List<T> newPool = new List<T>();
+       
+        for (int i = 0; i < count; i++)
+        {
+            GameObject projectileObject = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            T newProjectcile = projectileObject.GetComponent<T>();
+
+            newPool.Add(newProjectcile);
+        }
+
+        return null;
+    }
+
+
+
+
+
+}
+
+
+public class AC_ProjectilePools : Pool
+{
+    public List<AC_Projectiles> a_Projectile = new List<AC_Projectiles>();
+    public AC_ProjectilePools(GameObject prefab, int count)
+    {
+        a_Projectile = Create<AC_Projectiles>(prefab, count);
+    }
+    public void SetAllProjectiles()
+    {
+        foreach (AC_Projectiles projectile in a_Projectile)
+            projectile.SetInactive();
+    }
+}    
---
+++ Assets/Scripts/Level One Scripts/AC_Projectiles.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AC_Projectiles : MonoBehaviour
+{
+    public float o_Lifetime = 5.0f;
+    private Rigidbody o_Rigidbody = null;
+    public int o_Force = 10;
+
+
+    private void Awake()
+    {
+        o_Rigidbody = GetComponent<Rigidbody>();
+        SetInactive();
+    }
+
+    private void OnCollisionEnter(Collision collision) // projectile 
+    {
+        SetInactive();
+    }
+
+    public void Launch(AC_Shooter blaster)
+    {
+        //Position with player
+        transform.position = blaster.o_SpawnLocation.position;
+        transform.rotation = blaster.o_SpawnLocation.rotation;
+
+        //Activate
+        gameObject.SetActive(true);
+
+        //Fire, Force and Track Location
+        o_Rigidbody.AddRelativeForce(Vector3.forward * o_Force, ForceMode.Impulse);
+        StartCoroutine(TrackLifeTime());
+    }
+
+    private IEnumerator TrackLifeTime()
+    {
+        yield return new WaitForSeconds(o_Lifetime);
+        SetInactive();
+    }
+
+    public void SetInactive() //resets projectile to stop movement for new projectiles
+    {
+        o_Rigidbody.velocity = Vector3.zero;
+        o_Rigidbody.angularVelocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+
+
+}
