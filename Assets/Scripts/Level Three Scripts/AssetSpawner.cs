7e71ce75f5209630b8738f0834e04147f7b9f70f
Anne Caves <1014251@student.sae.edu.au>
2021-04-26 10:33:45 +1000

    Level Three Asset Spawner Script Added

---
+++ Assets/Scripts/Level Three Scripts/AssetSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetSpawner : MonoBehaviour
+{
+    public GameObject assetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        assetObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
